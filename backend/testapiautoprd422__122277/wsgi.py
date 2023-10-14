bf9890cbfcbff25616790e000fbfc2e55f482b69
Crowdbotics <engineering@crowdbotics.com>
2023-10-14 00:15:16 +0000

    Initial commit

---
+++ backend/testapiautoprd422__122277/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for testapiautoprd422__122277 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "testapiautoprd422__122277.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testapiautoprd422--122277",
+  url: "https://testapiautoprd422--122277.botics.co",
+  api: "https://testapiautoprd422--122277.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
